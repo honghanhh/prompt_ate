4075baff8dab2bd32ae56e8c72e2a703c29b7ebd
Hồng Hạnh <hanh.usth@gmail.com>
2023-08-26 16:18:38 +0200

    update prompt script

--- prompts/run_eval.sh
+++ prompts/run_eval.sh
@@ -1,9 +1,9 @@
-python evaluate.py --data_path ../results/en_htfl_preds.csv --lang en --ver ann 
-python evaluate.py --data_path ../results/en_htfl_preds.csv --lang en --ver nes
+python evaluate.py --data_path ../results/prompt_results/en_htfl_preds.csv --lang en --ver ann 
+python evaluate.py --data_path ../results/prompt_results/en_htfl_preds.csv --lang en --ver nes
 
 
-python evaluate.py --data_path ../results/fr_htfl_preds.csv --lang fr --ver ann 
-python evaluate.py --data_path ../results/fr_htfl_preds.csv --lang fr --ver nes 
+python evaluate.py --data_path ../results/prompt_results/fr_htfl_preds.csv --lang fr --ver ann 
+python evaluate.py --data_path ../results/prompt_results/fr_htfl_preds.csv --lang fr --ver nes 
 
-python evaluate.py --data_path ../results/nl_htfl_preds.csv --lang nl --ver ann 
-python evaluate.py --data_path ../results/nl_htfl_preds.csv --lang nl --ver nes 
+python evaluate.py --data_path ../results/prompt_results/nl_htfl_preds.csv --lang nl --ver ann 
+python evaluate.py --data_path ../results/prompt_results/nl_htfl_preds.csv --lang nl --ver nes 
--- prompts/run_prompt.sh
+++ prompts/run_prompt.sh
@@ -1,8 +1,8 @@
-python evaluate.py --data_path ../processed_data/en_htfl.csv --lang en --ver ann --output_path en_htfl_preds_ann.csv
-python evaluate.py --data_path ../processed_data/en_htfl.csv --lang en --ver nes --output_path en_htfl_preds_nes.csv
+python evaluate.py --data_path ../data/processed_data/en_htfl.csv --lang en --ver ann --output_path ../results/prompt_results/en_htfl_preds_ann.csv
+python evaluate.py --data_path ../data/processed_data/en_htfl.csv --lang en --ver nes --output_path ../results/prompt_results/en_htfl_preds_nes.csv
 
-python evaluate.py --data_path ../processed_data/fr_htfl.csv --lang fr --ver ann --output_path fr_htfl_preds_ann.csv
-python evaluate.py --data_path ../processed_data/fr_htfl.csv --lang fr --ver nes --output_path fr_htfl_preds_nes.csv
+python evaluate.py --data_path ../data/processed_data/fr_htfl.csv --lang fr --ver ann --output_path ../results/prompt_results/fr_htfl_preds_ann.csv
+python evaluate.py --data_path ../data/processed_data/fr_htfl.csv --lang fr --ver nes --output_path ../results/prompt_results/fr_htfl_preds_nes.csv
 
-python evaluate.py --data_path ../processed_data/nl_htfl.csv --lang nl --ver ann --output_path nl_htfl_preds_ann.csv
-python evaluate.py --data_path ../processed_data/nl_htfl.csv --lang nl --ver nes --output_path nl_htfl_preds_nes.csv
+python evaluate.py --data_path ../data/processed_data/nl_htfl.csv --lang nl --ver ann --output_path ../results/prompt_results/nl_htfl_preds_ann.csv
+python evaluate.py --data_path ../data/processed_data/nl_htfl.csv --lang nl --ver nes --output_path ../results/prompt_results/nl_htfl_preds_nes.csv
